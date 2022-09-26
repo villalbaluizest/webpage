bb5ad02e815e52274bb30fbf26ab62ccc31197f2
villalbaluis <luisvillalba244@gmail.com>
2022-09-26 17:49:49 -0500

    Add: Footer sectiong working

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 $(window).on("scroll", function() {
     if ($(".navbar").offset().top > 40) {
         $(".navbar").addClass("navbar-fixed");
+        $(".go-top").show();
     } else {
         $(".navbar").removeClass("navbar-fixed");
+        $(".go-top").hide();
     }
 })
 
@@ -28,7 +30,5 @@ $(document).ready(function() {
             navigateByImgClick: true
         }
     });
-
-
 
 });
